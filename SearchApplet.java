62deea70b30ffd130d703aafb4b7991aea5204fb
ramirogomez <ramirogomez>
2002-06-19 11:08:52 +0000

    Now the list of titles, of documents where matches were found, is displayed in the pop up frame.

--- SearchApplet.java
+++ SearchApplet.java
@@ -7,7 +7,13 @@ import java.awt.*;
 import java.awt.event.*;
 import java.applet.*;
 import java.net.*;
-import java.util.Vector;
+// Wenn 'import java.util.*' gibt es Namenskonflikte
+// SearchApplet.java:107: reference to List is ambiguous,
+// both class java.util.List in java.util 
+// and class java.awt.List in java.awt match
+
+//import java.util.Vector;
+import java.util.StringTokenizer;
 
 public class SearchApplet extends Applet implements ActionListener {
 
@@ -18,7 +24,7 @@ public class SearchApplet extends Applet implements ActionListener {
     private String chosenOpt = "All Words"; // 'All Words' default Option
     private String searchTerms;
     private Result result;
-    public Vector matches;
+    private String[] matches;
 
     public void init() {
 	setBackground(Color.yellow);
@@ -73,9 +79,7 @@ public class SearchApplet extends Applet implements ActionListener {
     public void actionPerformed (ActionEvent e) {
 	searchTerms = searchField.getText();
 	ReadIndex ri = new ReadIndex(chosenLang, chosenOpt, searchTerms); 
-	//System.out.println("Vorher: " + matches);
-	setMatches(ri.getHits());
-	//System.out.println("Nacher: "+ matches);
+	matches = (ri.getHitsArray());
 	
 	// Ergebnisfenster
 	result = new Result(getAppletContext(),this); // this = aktuelles Applet
@@ -95,57 +99,60 @@ public class SearchApplet extends Applet implements ActionListener {
 	}
     }
     
-    public void setMatches(Vector vec) {
-	matches = vec;
-    }
-    
-    public Vector getMatches() {
+    public String[] getMatches() {
 	return matches;
     }
-    
+   
 } // class SearchApplet    
 
 // Ergebnisse darstellen
 class Result extends Frame implements ActionListener {
-    SearchApplet searchApplet;
-    String chosenURL;
-    Label label;
-    List urlList;
-    Button go;
-    AppletContext appletContext;
+    private SearchApplet searchApplet;
+    private String chosenURL;
+    private Label label;
+    private List urlList;
+    private Button go;
+    private AppletContext appletContext;
     
-    /*
-      Brauche noch zwei Arrays:
-      Titel der docs
-      Urls der docs
-      Titel anzeigen lassen Urls wählen
-     */
+    /**
+       Die einzelnen Objekte des Vektors 'matches' mit einer StringTokenizer Instanz
+       in die beiden Komponenten 'url' und 'title' aufspalten und in die zugehörigen
+       Arrays 'urls' bzw. 'titles' einfügen. Das 'titles'-Array ist dann in der Liste
+       sichtbar. Bei Klick auf einen der Titel wird der zugehörige URL ausgewählt und
+       in dem aktuellen Browser-Fenster geöffnet.
+    */
     public Result(AppletContext appletContext, SearchApplet searchApplet) {
 	setLayout(new BorderLayout());
 	setSize(520,600);
         this.appletContext = appletContext;
 	this.searchApplet = searchApplet;
 	
-	label = new Label("Search Results", Label.CENTER);
+	String[] matches = searchApplet.getMatches();
+	final int TOTAL = matches.length;
+	StringTokenizer urlTitle;
+	String[] urls = new String[TOTAL];
+	String[] titles = new String[TOTAL];
+	
+	label = new Label(TOTAL + " documents found.", Label.CENTER);
 	label.setBackground(Color.white);
 	add(label, BorderLayout.NORTH);
 	
 	urlList = new List(20, false); // 20 Elemente sichtbar, eines auswählbar
         urlList.setBackground(Color.white);
 	
-	Vector matches = searchApplet.getMatches();
-	
-	// hier StringTokenizer verwenden und zwei Arrays (2 Arrays (url,title) und List füllen)
-	String[] urlTitle = new String[matches.size()];  
-	
-	for (int i=0; i<matches.size();i++) {
-	    urlTitle[i] = matches.get(i).toString();
-	    System.out.println(urlTitle[i]);
+	if (matches != null) {
+	    for (int i=0; i<TOTAL;i++) {
+		urlTitle = new StringTokenizer(matches[i],"|");
+		titles[i] = urlTitle.nextToken();
+		urls[i] = urlTitle.nextToken();
+		urlList.add(titles[i]);
+	    }
+	    urlList.addItemListener(new URLChoice());
 	}
-    	
-	urlList.add("index.html");
-        urlList.add("indexDE.html");
-	urlList.addItemListener(new URLChoice());
+	else {
+	    urlList.add("No matches found.");
+	}
+	
 	add(urlList, BorderLayout.CENTER);
 
 	go = new Button("Show document");
