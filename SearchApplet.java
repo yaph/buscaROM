c1d2e8d4da738739732b9590b2fad956dbd3ee56
ramirogomez <ramirogomez>
2002-06-20 15:19:38 +0000

    Again lots of changes, but boolean search is still not implemented. The applet has to be signed to open the index files.

--- SearchApplet.java
+++ SearchApplet.java
@@ -6,6 +6,8 @@
 import java.awt.*;
 import java.awt.event.*;
 import java.applet.Applet;
+import java.util.StringTokenizer;
+import java.util.Vector;
 
 public class SearchApplet extends Applet implements ActionListener {
 
@@ -13,8 +15,9 @@ public class SearchApplet extends Applet implements ActionListener {
     private TextField searchField;
     private Button searchButton;
     private String chosenLang = "German"; // 'German' default Sprache
-    private String chosenOpt = "All Words"; // 'All Words' default Option
-    private String searchTerms;
+    private final String AND = "All Words";
+    private final String OR = "Any Word";
+    private String chosenOpt = "AND"; // 'All Words' default Option
     private Result result;
     private String[] urls, titles;
     
@@ -51,8 +54,8 @@ public class SearchApplet extends Applet implements ActionListener {
 	searchOptions = new Choice();
 	searchOptions.setBackground(Color.gray);
 	searchOptions.setBounds(105,40,100,30);
-	searchOptions.addItem("All Words");
-	searchOptions.addItem("Any Word");
+	searchOptions.addItem("AND");
+	searchOptions.addItem("OR");
 	searchOptions.addItemListener(new OptionChoice());
 	add(searchOptions);
     } // init()
@@ -69,17 +72,47 @@ public class SearchApplet extends Applet implements ActionListener {
        Reaktion auf Benutzer Interaktion
     */
     public void actionPerformed (ActionEvent e) {
-	searchTerms = searchField.getText();
+	String searchTerm = searchField.getText();;
+	final String WS = " "; // ' ' trennt mehrere Suchbegriffe
+	ReadIndex ri;
 	
-	ReadIndex ri = new ReadIndex(chosenLang, chosenOpt, searchTerms); 
-	
-	urls = ri.getURLs();
-	titles = ri.getTitles();
-
-	// Ergebnisfenster
-	result = new Result(getAppletContext(),this); // this = aktuelles Applet
-        result.pack(); // Fenstergröße anhand der enthaltenen Komponenten bestimmen
-	result.setVisible(true);
+	if (searchTerm.length() > 0) { // nur wenn etwas eingegeben wurde reagieren
+	    if (searchTerm.indexOf(WS) > 0) { // mehr als ein Suchwort
+		StringTokenizer words = new StringTokenizer(searchTerm, WS);
+		//final int TOTAL = words.countTokens();
+		//Vector urlVec = new Vector();
+		//Vector titleVec = new Vector();
+		
+		int wordCount = 0;
+		int urlCount = 0;
+		while (words.hasMoreTokens()) {
+		    System.out.println(words.nextToken());
+		    ri = new ReadIndex(chosenLang, words.nextToken());
+		    ri.getMatches();
+		    urls = ri.getURLs();
+		    titles = ri.getTitles();
+		    urlCount += urls.length;
+		    wordCount++;
+		}
+		System.out.println(urlCount);
+		
+		if (chosenOpt.equals(AND)) {
+		    //ri.andSearch();
+		} 
+		else {
+		}
+	    } 
+	    else { // nur ein Suchwort AND, OR egal
+		ri = new ReadIndex(chosenLang, searchTerm); 
+		ri.getMatches();
+		urls = ri.getURLs();
+		titles = ri.getTitles();
+	    }
+	    
+	    // Ergebnisfenster
+	    result = new Result(getAppletContext(),this); // this = aktuelles Applet
+	    result.setVisible(true);
+	} // if (searchTerms.length() > 0)
     }
     
     private class LanguageChoice implements ItemListener {
