4b00007350f13fdb12cf48af0972ceff177e7f51
ramirogomez <ramirogomez>
2002-06-25 11:57:27 +0000

    Now boolean "AND" search works, at least with the search terms I entered.

--- SearchApplet.java
+++ SearchApplet.java
@@ -96,22 +96,26 @@ public class SearchApplet extends Applet implements ActionListener {
 		for (int i = 0; i < TOTAL; i++) {
 		    ReadIndex ri = new ReadIndex(chosenLang, words.nextToken());
 		    ri.getMatches();
-		    urlCount[i] = ri.getUrlCount();// Anzahl der URLs (=Titel)
-		    urlVec.add(ri.getURLs());
-		    titleVec.add(ri.getTitles());
-		    totalUrlCount += urlCount[i];
+		    int uCount = ri.getUrlCount();// Anzahl der URLs (=Titel)
+		    
+		    // Wenn uCount '0' ist, dann ist das Array leer
+		    if (uCount > 0) { 
+			urlCount[i] = uCount;
+			urlVec.add(ri.getURLs());
+			titleVec.add(ri.getTitles());
+			totalUrlCount += urlCount[i];
+		    }
 		}
 		
 		// Nur wenn Ergebnisvektor nicht leer ist
-		if (urlVec.capacity() > 1) {
+		if (!urlVec.isEmpty()) {
 		    
 		    // UND Suche
 		    if (chosenOpt.equals("AND")) {
 			/*
 			  Das kürzeste Array finden und nur die Elemente
-			  dieses Array mit den Elementen der anderen Vergleichen
-			  (Hashtable???). Was im Gesamtergebnis ist muss auch im
-			  kürzesten sein.
+			  dieses Arrays mit den Elementen der anderen Vergleichen. 
+			  Was im Gesamtergebnis ist muss auch im kürzesten Array sein.
 			*/
 			int minAr = 0; // Zunächst erstes Array auf minimum gestetzt
 			int min = 1000; // hoher Anfangswert
@@ -121,32 +125,127 @@ public class SearchApplet extends Applet implements ActionListener {
 				minAr = i;
 			    }
 			}
-
+			
 			/*
-			  Die Elemente des kürzesten Arrays in Hashtable speichern. 
-			  Die URLs sind die Keys, da eindeutig.
+			  Die Elemente des kürzesten Arrays werden in der Hashtable
+			  cmpHash gespeichert. Die 'Keys' sind die URLs, da sie eindeutig sind.
+			  Jedes Element aller 'urls'-Arrays (mit Ausnahme des Kürzesten) wird
+			  geprüft, ob es in cmpHash vorhanden ist. Ist es vorhanden wird der
+			  aktuelle URL und der dazugehörige Titel in resultHash gespeichert.
+			  Ist nach der 1. Iteration kein Eintrag in resultHash vorhanden, wird
+			  die Suche beendet, da kein Dokument die ersten beiden verglichenen
+			  Suchbegriffe enthält. Weitere Suchbegriffe sind dann unerheblich.
 			*/
 			Hashtable cmpHash = new Hashtable();
+			Hashtable resultHash = new Hashtable();
 			String[] minArray = new String[min];
 			minArray = (String[]) urlVec.elementAt(minAr);
+			
+			// Hash mit URLs aus minArray füllen
 			for (int i = 0; i < min; i++) {
 			    cmpHash.put(minArray[i], "1"); // Nur an den Keys interessiert
 			}
 			
-			urls = new String[totalUrlCount]; //erstmal zu viel
-			titles = new String[totalUrlCount]; //erstmal zu viel
-			
-			
-			//System.out.println("Min: " + min + "\nMinaAr: " + minAr);
-			
 			/* 
-			   Die URLs des kürzesten Arrays in eine Hashtable speichern.
-			   Die Urls aller anderen Arrays daraufhin prüfen, ob sie bereits
-			   in der Hashtabelle vorhanden sind.
+			   Flag 'result'. Zunächst wird davon ausgegangen, dass es ein 
+			   Ergebnis gibt. Wenn nicht wird der Vergleich beendet. 'cmpCount'
+			   wird um 1 erhöht, wenn ein Array mit den Hashkeys verglichen
+			   wurde. Ist 'cmpCount' größer 0 und resultHash leer, wird der
+			   Vergleich beendet, da kein Dokument alle Suchbegriffe enthalten
+			   kann.
 			*/
+			boolean result = true;
+			int cmpCount = 0;
 			
-			//Hashtable compareHash = new Hashtable;
-			//if containsKey
+			// Vergleich der anderen Arrays mit cmpHash
+			for (int i = 0; i < urlCount.length; i++) {
+			    if (result) { // Vergleich ausführen
+				if (i != minAr) { // kürzestes Array auslassen
+				    /* 
+				       Wenn schon ein Vergleich dürchgeführt wurde und
+				       'resultHash' nicht leer ist.
+				    */
+				    if (cmpCount > 0 && resultHash.size() != 0) {
+					String[] urlAr = new String[urlCount[i]];
+					urlAr = (String[]) urlVec.elementAt(i);
+					
+					cmpCount++;
+					for (int j = 0; j < urlAr.length; j++) {
+					    /*
+					      Wenn 'Key' in cmpHash aber nicht in resultHash
+					      vorhanden, 'key' aus 'cmpHash'. Sonst so belassen.
+					    */
+					    if (cmpHash.containsKey(urlAr[j]) && !resultHash.containsKey(urlAr[j])) {
+						cmpHash.remove(urlAr[j]);
+					    }
+					} // for (int j = 0; j < urlAr.length; j++)
+				    } // if (cmpCount > 0 && resultHash.size != 0)
+				    
+				    /*
+				      Wenn noch kein Vergleich dürchgeführt wurde über alle
+				      Elemente des Arrays iterieren. Wenn ein Element in 'cmpHash'
+				      vorhanden ist, dann 'url' und 'Title' in resultHash speichern.
+				    */
+				    else if (cmpCount == 0) {
+					String[] urlAr = new String[urlCount[i]];
+					String[] titleAr = new String[urlCount[i]];
+					urlAr = (String[]) urlVec.elementAt(i);
+					titleAr = (String[]) titleVec.elementAt(i);
+					
+					cmpCount++;
+					for (int j = 0; j < urlAr.length; j++) {
+					    /*
+					      Wenn 'Key' in cmpHash dann 'url' und 'title' in
+					      resultHash speichern.
+					    */
+					    if (cmpHash.containsKey(urlAr[j])) {
+						resultHash.put(urlAr[j], titleAr[j]);
+					    }
+					}
+				    } // else if (cmpCount == 0)
+				    
+				    else { // sonst kein Treffer
+					result = false;
+				    } // else
+				} // if (i != minAr)
+			    } // if (result)
+			} // for (int i = 0; i < urlCount.length; i++)
+			
+			/*
+			  Wenn es ein Ergebnis gibt die 'keys' von resultHash
+			  im Array 'urls' speichern und die 'values' im Array
+			  'titles'.
+			*/
+			if (result) {
+			    String[] urlAr = new String[min];
+			    String[] titleAr = new String[min];
+			    urlAr = (String[]) urlVec.elementAt(minAr);
+			    titleAr = (String[]) urlVec.elementAt(minAr);
+			    
+			    /*
+			      Ergebnisarrays füllen. Da es keine Methode gibt, die
+			      alle 'keys' zurück gibt, werden die Elemente des kürzesten
+			      Arrays 'urlAr[minAr]' mit den 'keys' in resultHash vergleichen.
+			      Wenn ein Element als 'key' vorhanden ist, wird der 'key' als 
+			      in Ergebnisarray 'urls' und der 'value' im Ergebnisarray
+			      'titles' gespeichert. 
+			    */
+			    urls = new String[resultHash.size()];
+			    titles = new String[resultHash.size()];
+			    int resultCount = 0; // nicht unbedingt gleich 'i' in for Schleife
+			    for (int i = 0; i < urlAr.length; i++) {
+				if (resultHash.containsKey(urlAr[i])) {
+				    urls[resultCount] = urlAr[i];
+				    titles[resultCount] = (String) resultHash.get(urlAr[i]);
+				    resultCount++;
+				}
+			    }
+			} // if (result) 
+			
+			else { // bei 0 Treffern leere Arrays
+			    urls = new String[0];
+			    titles = new String[0];
+			}
 			
 		    } // UND Suche 
 		    
@@ -170,29 +269,27 @@ public class SearchApplet extends Applet implements ActionListener {
 			}
 			Arrays.sort(urls);
 			Arrays.sort(titles);
-		    }
-		} 
+		    } // OR
+		} // if (urlVec.capacity() > 1)
 		
-		else { // nur ein Suchwort AND, OR egal
-		    ReadIndex ri = new ReadIndex(chosenLang, searchTerm); 
-		    ri.getMatches();
-		    urls = ri.getURLs();
-		    titles = ri.getTitles();
+		else { // bei 0 Treffern leere Arrays
+		    urls = new String[0];
+		    titles = new String[0];
 		}
-	    } // if (urlVec.capacity() > 1) {
-
-	    else { // Ausgabe bei 0 Treffern
-		urls = new String[1];
-		urls[0] = "";
-		titles = new String[1];
-		titles[0] = "No matches found";
+	    }
+	    
+	    else { // nur ein Suchwort AND, OR egal
+		ReadIndex ri = new ReadIndex(chosenLang, searchTerm); 
+		ri.getMatches();
+		urls = ri.getURLs();
+		titles = ri.getTitles();
 	    }
 	    
 	    // Ergebnisfenster
 	    result = new Result(getAppletContext(),this); // this = aktuelles Applet
 	    result.setVisible(true);
 	} // if (searchTerms.length() > 0)
-    }
+    } // public void actionPerformed (ActionEvent e)
     
     private class LanguageChoice implements ItemListener {
 	public void itemStateChanged(ItemEvent e) {
