21ae3fa7e64b7822a61ecf412d8865f2af2b1d81
ramirogomez <ramirogomez>
2002-08-12 12:47:14 +0000

    Removed Option to choose the language.

--- ReadIndex.java
+++ ReadIndex.java
@@ -6,60 +6,43 @@ import java.net.URL;
 import java.net.MalformedURLException;
 
 public class ReadIndex {
-    private String chosenLang, chosenOpt, searchTerm;
+    private String chosenOpt, searchTerm;
     private Vector hits = new Vector();
     private int numberHits; // Anzahl der Treffer
     private URL indexFile;
     final private String SEPARATOR = "|";
     private String[] urls, titles;
-    private URL baseDir; 
-	
+    private URL baseDir;
+
     /**
        <h1>Konstruktor:</h1>
-       Die gewählte Sprache und das Suchwort werden dem Konstruktor übergeben.
-       Die Sprachauswahl legt fest, welcher Wert der Variablen indexFile zugewiesen wird.
+       Das Suchwort wird dem Konstruktor übergeben.
        Das Suchwort wird einer String-Instanz zugewiesen.
     */
-    public ReadIndex(URL dir, String lang, String term) {
+    public ReadIndex(URL dir, String term) {
 	baseDir = dir;
-	chosenLang = lang;
 	searchTerm = term;
 	
-	if (chosenLang.equals("German")) {
-	    try {
-		indexFile = new URL(baseDir,"indexDE.dat");
-	    }
-	    catch (MalformedURLException ex) {
-		System.out.println(ex);
-            }
-	} else if (chosenLang.equals("French")) {
-	    try {
-		indexFile = new URL(baseDir,"indexFR.dat");
-	    }
-	    catch (MalformedURLException ex) {
-		System.out.println(ex);
-            }
-	} else {
-	    try {
-		indexFile = new URL(baseDir,"indexIT.dat");
-	    }
-	    catch (MalformedURLException ex) {
-		System.out.println(ex);
-            }
+	try {
+	    indexFile = new URL(baseDir,"index.dat");
 	}
+	catch (MalformedURLException ex) {
+	    System.out.println(ex);
+	}
+	
     } // ReadIndex()
-    
+
     /**
        Die Indexdatei wird eingelesen und Zeilenweise abgearbeitet.
        Jede Zeile wird zunächst in die drei Variablen 'url', 'title' und 'keyword'
        aufgeteilt (Separator ist '|'). Die Zeichenkette 'keyword' wird wiederum in die
        Stringvariable 'keywords' aufgeteilt. Die einzelnen Tokens dieses Strings werden
-       mit den Suchbegriffen verglichen. Bei einem Treffer, werden 'title' und 'url' 
-       dem Vector 'hits' zusammen als eine Stringinstanz 'hits' angefügt. Der Vector 
-       ist als Datenstruktur in Java dynamisch veränderbar. Da die Trefferanzahl nicht von 
-       vornherein feststeht bietet er eine unkomplizierte Möglichkeit die Treffer zu speichern. 
+       mit den Suchbegriffen verglichen. Bei einem Treffer, werden 'title' und 'url'
+       dem Vector 'hits' zusammen als eine Stringinstanz 'hits' angefügt. Der Vector
+       ist als Datenstruktur in Java dynamisch veränderbar. Da die Trefferanzahl nicht von
+       vornherein feststeht bietet er eine unkomplizierte Möglichkeit die Treffer zu speichern.
        Nachdem die Indexdatei abgearbeitet ist, werden die einzelnen Objekte des Vektors 'hits'
-       mit einer StringTokenizer Instanz wieder in die beiden Komponenten aufspalten und in den 
+       mit einer StringTokenizer Instanz wieder in die beiden Komponenten aufspalten und in den
        Arrays 'titles' und 'urls' gespeichert.
     */
     public void getMatches() {
@@ -67,7 +50,7 @@ public class ReadIndex {
 	    InputStreamReader isr = new InputStreamReader(indexFile.openStream());
 	    BufferedReader inFile = new BufferedReader(isr);
 	    String line = inFile.readLine();
-	    
+
 	    while (line != null) {
 		StringTokenizer fields = new StringTokenizer (line,SEPARATOR);
 		String url = "", title = "", keyword = "";
@@ -79,7 +62,7 @@ public class ReadIndex {
 		catch (NoSuchElementException exception) {
 		    System.out.println("URL: "+url + "Title: " +title + "key: " +keyword + exception);
 		}
-		
+
 		if (keyword != null) {
 		    StringTokenizer keywords = new StringTokenizer(keyword);
 		    while (keywords.hasMoreTokens()) {
@@ -102,7 +85,7 @@ public class ReadIndex {
 	catch (SecurityException exception) {
 	    System.out.println("Security problem" + exception);
 	}
-	
+
 	// Hier CASTen!!!
 	if (hits != null) {
 	    final int TOTAL = hits.size();
@@ -110,28 +93,28 @@ public class ReadIndex {
 	    String[] strArray = new String[TOTAL];
 	    titles = new String[TOTAL];
 	    urls = new String[TOTAL];
-	    
+
 	    for (int i = 0; i < TOTAL; i++) {
 		strArray[i] = objArray[i].toString();
 	    }
-	    
+
 	    Arrays.sort(strArray);
-	    
+
 	    for (int i = 0; i < TOTAL; i++) {
 		StringTokenizer urlTitle = new StringTokenizer(strArray[i], SEPARATOR);
 		titles[i] = urlTitle.nextToken();
 		urls[i] = urlTitle.nextToken();
 	    }
-	} 	
+	}
     } // getMatches()
-    
+
     /**
        Gibt ein Array mit den URLs der Treffer zurück.
     */
     public String[] getURLs() {
 	return urls;
     }
-    
+
     /**
        Gibt ein Array mit den Titeln der Treffer zurück.
     */
--- SearchApplet.java
+++ SearchApplet.java
@@ -1,10 +1,7 @@
 /**
-   Das Applet für die Suche im Eurodelphes Index. Je Sprache (Deutsch,
-   Franzöisch und Italienisch) existiert ein eigener Index. Der Nutzer 
-   muss bei der Eingabe die Sprache wählen, ansonsten wird deutsch als 
-   Defaultwert benutzt. Der Nutzer kann außerdem zwischen den Optionen
-   'All Words' (logisches UND) und 'Any Word' (logisches ODER) zur
-   Verknüpfungen der Suchbegriffe wählen.
+   Das Applet für die Suche im Eurodelphes Index. Der Nutzer kann
+   zwischen den Optionen 'All Words' (logisches UND) und 'Any Word'
+   (logisches ODER) zur Verknüpfungen der Suchbegriffe wählen.
 */
 import java.awt.*;
 import java.awt.event.*;
@@ -17,23 +14,22 @@ import java.net.URL;
 
 public class SearchApplet extends Applet implements ActionListener {
 
-    private Choice language, searchOptions;
+    private Choice searchOptions;
     private TextField searchField;
     private Button searchButton;
-    private String chosenLang = "German"; // 'German' default Sprache
     private String chosenOpt = "AND"; // 'AND' default Option
     private Result result;
     private String[] urls, titles;
     private URL baseDir;
-    
+
     public void init() {
 	baseDir = getCodeBase();
-	
+
 	setBackground(Color.yellow);
-	
+
 	// individuelle Positionierung der Komponenten (Layout)
 	setLayout(null);
-	
+
 	searchField = new TextField(20);
 	searchField.setBackground(Color.white);
 	searchField.setBounds(5,5,200,30);
@@ -42,20 +38,10 @@ public class SearchApplet extends Applet implements ActionListener {
 
 	searchButton = new Button("Search!");
 	searchButton.setBackground(Color.red);
-	searchButton.setBounds(210,5,60,30);
+	searchButton.setBounds(210,5,65,30);
 	searchButton.addActionListener(this);
 	add(searchButton);
 
-	// Auswahlliste Sprachen
-	language = new Choice();
-	language.setBackground(Color.gray);
-	language.setBounds(5,40,100,30);
-	language.addItem("German");
-	language.addItem("French");
-	language.addItem("Italian");
-	language.addItemListener(new LanguageChoice());
-	add(language);
-	
 	// Auswahlliste Suchoptionen
 	// Phrasensuche aufgrund des Indexes nicht möglich
 	searchOptions = new Choice();
@@ -66,7 +52,7 @@ public class SearchApplet extends Applet implements ActionListener {
 	searchOptions.addItemListener(new OptionChoice());
 	add(searchOptions);
     } // init()
-    
+
     /**
        Result frame schließen und Result Instanz zerstören
     */
@@ -81,7 +67,7 @@ public class SearchApplet extends Applet implements ActionListener {
     public void actionPerformed (ActionEvent e) {
 	String searchTerm = searchField.getText();;
 	final String WS = " "; // ' ' trennt mehrere Suchbegriffe
-	
+
 	if (searchTerm.length() > 0) { // nur wenn etwas eingegeben wurde reagieren
 	    if (searchTerm.indexOf(WS) > 0) { // mehr als ein Suchwort
 		StringTokenizer words = new StringTokenizer(searchTerm, WS);
@@ -90,33 +76,33 @@ public class SearchApplet extends Applet implements ActionListener {
 		int totalUrlCount = 0;
 		Vector urlVec = new Vector();
 		Vector titleVec = new Vector();
-		
-		/* 
+
+		/*
 		   Für jedes Suchwort die Ergebnissarrays holen und zum den
 		   Ergebnisvektoren 'urlVec' und 'titleVec' zufügen.
 		*/
 		for (int i = 0; i < TOTAL; i++) {
-		    ReadIndex ri = new ReadIndex(baseDir, chosenLang, words.nextToken());
+		    ReadIndex ri = new ReadIndex(baseDir, words.nextToken());
 		    ri.getMatches();
 		    int uCount = ri.getUrlCount();// Anzahl der URLs (=Titel)
-		    
+
 		    // Wenn uCount '0' ist, dann ist das Array leer
-		    if (uCount > 0) { 
+		    if (uCount > 0) {
 			urlCount[i] = uCount;
 			urlVec.add(ri.getURLs());
 			titleVec.add(ri.getTitles());
 			totalUrlCount += urlCount[i];
 		    }
 		}
-		
+
 		// Nur wenn Ergebnisvektor nicht leer ist
 		if (!urlVec.isEmpty()) {
-		    
+
 		    // UND Suche
 		    if (chosenOpt.equals("AND")) {
 			/*
 			  Das kürzeste Array finden und nur die Elemente
-			  dieses Arrays mit den Elementen der anderen Vergleichen. 
+			  dieses Arrays mit den Elementen der anderen Vergleichen.
 			  Was im Gesamtergebnis ist muss auch im kürzesten Array sein.
 			*/
 			int minAr = 0; // Zunächst erstes Array auf minimum gestetzt
@@ -127,7 +113,7 @@ public class SearchApplet extends Applet implements ActionListener {
 				minAr = i;
 			    }
 			}
-			
+
 			/*
 			  Die Elemente des kürzesten Arrays werden in der Hashtable
 			  cmpHash gespeichert. Die 'Keys' sind die URLs, da sie eindeutig sind.
@@ -142,14 +128,14 @@ public class SearchApplet extends Applet implements ActionListener {
 			Hashtable resultHash = new Hashtable();
 			String[] minArray = new String[min];
 			minArray = (String[]) urlVec.elementAt(minAr);
-			
+
 			// Hash mit URLs aus minArray füllen
 			for (int i = 0; i < min; i++) {
 			    cmpHash.put(minArray[i], "1"); // Nur an den Keys interessiert
 			}
-			
-			/* 
-			   Flag 'result'. Zunächst wird davon ausgegangen, dass es ein 
+
+			/*
+			   Flag 'result'. Zunächst wird davon ausgegangen, dass es ein
 			   Ergebnis gibt. Wenn nicht wird der Vergleich beendet. 'cmpCount'
 			   wird um 1 erhöht, wenn ein Array mit den Hashkeys verglichen
 			   wurde. Ist 'cmpCount' größer 0 und resultHash leer, wird der
@@ -158,19 +144,19 @@ public class SearchApplet extends Applet implements ActionListener {
 			*/
 			boolean result = true;
 			int cmpCount = 0;
-			
+
 			// Vergleich der anderen Arrays mit cmpHash
 			for (int i = 0; i < urlCount.length; i++) {
 			    if (result) { // Vergleich ausführen
 				if (i != minAr) { // kürzestes Array auslassen
-				    /* 
+				    /*
 				       Wenn schon ein Vergleich dürchgeführt wurde und
 				       'resultHash' nicht leer ist.
 				    */
 				    if (cmpCount > 0 && resultHash.size() != 0) {
 					String[] urlAr = new String[urlCount[i]];
 					urlAr = (String[]) urlVec.elementAt(i);
-					
+
 					cmpCount++;
 					for (int j = 0; j < urlAr.length; j++) {
 					    /*
@@ -182,7 +168,7 @@ public class SearchApplet extends Applet implements ActionListener {
 					    }
 					} // for (int j = 0; j < urlAr.length; j++)
 				    } // if (cmpCount > 0 && resultHash.size != 0)
-				    
+
 				    /*
 				      Wenn noch kein Vergleich dürchgeführt wurde über alle
 				      Elemente des Arrays iterieren. Wenn ein Element in 'cmpHash'
@@ -193,7 +179,7 @@ public class SearchApplet extends Applet implements ActionListener {
 					String[] titleAr = new String[urlCount[i]];
 					urlAr = (String[]) urlVec.elementAt(i);
 					titleAr = (String[]) titleVec.elementAt(i);
-					
+
 					cmpCount++;
 					for (int j = 0; j < urlAr.length; j++) {
 					    /*
@@ -205,14 +191,14 @@ public class SearchApplet extends Applet implements ActionListener {
 					    }
 					}
 				    } // else if (cmpCount == 0)
-				    
+
 				    else { // sonst kein Treffer
 					result = false;
 				    } // else
 				} // if (i != minAr)
 			    } // if (result)
 			} // for (int i = 0; i < urlCount.length; i++)
-			
+
 			/*
 			  Wenn es ein Ergebnis gibt die 'keys' von resultHash
 			  im Array 'urls' speichern und die 'values' im Array
@@ -223,14 +209,14 @@ public class SearchApplet extends Applet implements ActionListener {
 			    String[] titleAr = new String[min];
 			    urlAr = (String[]) urlVec.elementAt(minAr);
 			    titleAr = (String[]) urlVec.elementAt(minAr);
-			    
+
 			    /*
 			      Ergebnisarrays füllen. Da es keine Methode gibt, die
 			      alle 'keys' zurück gibt, werden die Elemente des kürzesten
 			      Arrays 'urlAr[minAr]' mit den 'keys' in resultHash vergleichen.
-			      Wenn ein Element als 'key' vorhanden ist, wird der 'key' als 
+			      Wenn ein Element als 'key' vorhanden ist, wird der 'key' als
 			      in Ergebnisarray 'urls' und der 'value' im Ergebnisarray
-			      'titles' gespeichert. 
+			      'titles' gespeichert.
 			    */
 			    urls = new String[resultHash.size()];
 			    titles = new String[resultHash.size()];
@@ -242,15 +228,15 @@ public class SearchApplet extends Applet implements ActionListener {
 				    resultCount++;
 				}
 			    }
-			} // if (result) 
-			
+			} // if (result)
+
 			else { // bei 0 Treffern leere Arrays
 			    urls = new String[0];
 			    titles = new String[0];
 			}
-			
-		    } // UND Suche 
-		    
+
+		    } // UND Suche
+
 		    else { // OR
 			urls = new String[totalUrlCount];
 			titles = new String[totalUrlCount];
@@ -259,10 +245,10 @@ public class SearchApplet extends Applet implements ActionListener {
 			    // Interims String Arrays
 			    String[] urlAr = new String[urlCount[i]];
 			    String[] titleAr = new String[urlCount[i]];
-			    
+
 			    urlAr = (String[]) urlVec.elementAt(i);
 			    titleAr = (String[]) titleVec.elementAt(i);
-			    
+
 			    for (int j = 0; j < urlAr.length; j++) {
 				urls[counter] = urlAr[j];
 				titles[counter] = titleAr[j];
@@ -273,44 +259,39 @@ public class SearchApplet extends Applet implements ActionListener {
 			Arrays.sort(titles);
 		    } // OR
 		} // if (urlVec.capacity() > 1)
-		
+
 		else { // bei 0 Treffern leere Arrays
 		    urls = new String[0];
 		    titles = new String[0];
 		}
 	    }
-	    
+
 	    else { // nur ein Suchwort AND, OR egal
-		ReadIndex ri = new ReadIndex(baseDir, chosenLang, searchTerm); 
+		ReadIndex ri = new ReadIndex(baseDir, searchTerm);
 		ri.getMatches();
 		urls = ri.getURLs();
 		titles = ri.getTitles();
 	    }
-	    
+
 	    // Ergebnisfenster
 	    result = new Result(getAppletContext(),this); // this = aktuelles Applet
 	    result.setVisible(true);
 	} // if (searchTerms.length() > 0)
     } // public void actionPerformed (ActionEvent e)
-    
-    private class LanguageChoice implements ItemListener {
-	public void itemStateChanged(ItemEvent e) {
-	    chosenLang = e.getItem().toString();
-	}
-    }
+
 
     private class OptionChoice implements ItemListener {
 	public void itemStateChanged(ItemEvent e) {
 	    chosenOpt = e.getItem().toString();
 	}
     }
-    
+
     public String[] getURLs() {
 	return urls;
     }
-    
+
     public String[] getTitles() {
 	return titles;
     }
-   
-} // class SearchApplet    
+
+} // class SearchApplet
